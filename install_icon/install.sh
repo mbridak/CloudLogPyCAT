628339bbf607889237cd2b888b25e2efd24d9ca3
mbridak <michael.bridak@gmail.com>
2021-05-14 09:48:09 -0700

    icon installer

---
+++ install_icon/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource install --size 64 --context apps --mode user k6gte-cloudcat.png k6gte-cloudcat
+
+xdg-desktop-icon install k6gte-CloudLogPyCAT.desktop
+
+xdg-desktop-menu install k6gte-CloudLogPyCAT.desktop
+
