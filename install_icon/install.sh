4be77a6fb0ee2df27b66ec5876c69cf1b7a6b3db
mbridak <michael.bridak@gmail.com>
2022-04-03 13:06:52 -0700

    Install.sh to copy  binary to .local/bin

--- install_icon/install.sh
+++ install_icon/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -f "../dist/CloudLogPyCAT" ]; then
+	cp ../dist/CloudLogPyCAT ~/.local/bin/
+fi
+
 xdg-icon-resource install --size 64 --context apps --mode user k6gte-cloudcat.png k6gte-cloudcat
 
 xdg-desktop-icon install k6gte-CloudLogPyCAT.desktop
